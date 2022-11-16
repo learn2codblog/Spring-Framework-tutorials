c1e75d1669b09a0a1cfd26654ed3c26ad1743711
Pradeep Kumar <itspradeepwbut@gmail.com>
2022-11-16 21:25:22 +0530

    Welcome to Spring framework tutorials

---
+++ src/main/java/com/learn2cod/springFrameWork/App.java
@@ -0,0 +1,18 @@
+package com.learn2cod.springFrameWork;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        
+        String hello="Hello welcome to Spring learning course";
+        
+        System.out.println( hello);
+    }
+    
+}
